9a0cfc52655740c1e7b6549ad37b4f2bf6920b8e
Justin VanDenHurk <justin.t.hurk@gmail.com>
2022-06-11 20:07:01 +0930

    libft

---
+++ ft_memset.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: justin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/11 19:51:26 by justin            #+#    #+#             */
+/*   Updated: 2022/06/11 20:06:41 by justin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t i;
+
+	i = -1;
+	while (i < n)
+	{
+		unsigned char	*s[i] = unsigned char	c;
+		i++;
+	}
+	return (s);
+}
+
+int main()
+{
+	//must test
+}
