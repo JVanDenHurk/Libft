310a97a972a0e9e55935ae748b425a8c98f96e19
Justin VanDenHurk <justin.t.hurk@gmail.com>
2022-07-02 12:22:38 +0930

    commit

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: justin <justin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 19:33:52 by justin            #+#    #+#             */
-/*   Updated: 2022/07/02 11:52:45 by justin           ###   ########.fr       */
+/*   Updated: 2022/07/02 12:06:55 by justin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@
 
 void    *ft_memcpy(void *dst, const void *src, size_t n)
 {
+    unsigned char *d;
+    const unsigned char *s;
     size_t i;
 
     if(!dst)
         return (NULL);
+    d = dst;
+    s = src;
     i = 0;
     while (i < n)
     {
-        *(char *)(dst  + i) = *(char *)(src + i);
+        d[i] = s[i];
         i++;
     }
     return (dst);
--- ft_memmov.c
+++ ft_memmov.c
@@ -6,13 +6,35 @@
 /*   By: justin <justin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 19:49:03 by justin            #+#    #+#             */
-/*   Updated: 2022/07/02 00:42:48 by justin           ###   ########.fr       */
+/*   Updated: 2022/07/02 12:22:11 by justin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// copies n bytes of string and removes memory blocks afterwards
 
 #include "libft.h"
 
 void    *ft_memmov(void *dst, const void *src, size_t len)
 {
-    
+    unsigned char *d;
+    const unsigned char *s;
+    size_t i;
+
+    if(!dst)
+        return (NULL);
+    d = dst;
+    s = src;
+    i = 0;
+    while (i < len)
+    {
+        d[i] = s[i];
+        i++;
+    }
+    i = len;
+    while (i > 0)
+    {
+        d[i - 1] = s[i - 1];
+        i--;
+    }
+    return (dst);
 }
