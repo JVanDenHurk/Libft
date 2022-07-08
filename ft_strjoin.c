3557e8d06fa7538eb4c75dc43eb44d3a5f4c4b62
Justin VanDenHurk <justin.t.hurk@gmail.com>
2022-07-08 10:37:55 +0930

    commit

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jvan-den <jvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 09:46:23 by jvan-den          #+#    #+#             */
-/*   Updated: 2022/07/08 10:13:59 by jvan-den         ###   ########.fr       */
+/*   Updated: 2022/07/08 10:37:21 by jvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,10 @@
 char *ft_strjoin(char const *s1, char const *s2)
 {
     char *str;
-    char * size;
     size_t i;
     size_t i2;
     
-    //str = (char*)malloc(sizeof((*s1) * (ft_strlen(s1) + ft_strlen(s2 + 1))));
-    *size = *s1 * (ft_strlen(s1) + ft_strlen(s2));
-    str = (char*)malloc(sizeof(size));
+    str = (char*)malloc(sizeof((*s1) * (ft_strlen(s1) + ft_strlen(s2))));
 
     if(!str)
         return (NULL);
@@ -31,26 +28,17 @@ char *ft_strjoin(char const *s1, char const *s2)
     i2 = 0;
     while (s1[i])
     {
-        str[i2++] = s1[i];
+        str[i2] = s1[i];
         i++;
+        i2++;
     }
     i = 0;
     while (s2[i])
     {
         str[i2] = s2[i];
         i++;
+        i2++;
     }
     str[i2] = 0;
     return (str);
-}
-
-int main()
-{
-    char s1[10] = "attack";
-    char s2[10] = "attack";
-    char ret;
-
-    ret = ft_strchr(s1, s2);
-
-    printf("%s\n", ret);
 }
