cde6f257f4e0aa03c0d335d0c5a138cfd55d87c4
Justin Van Den Hurk <Justin.t.hurk@gmail.com>
2022-07-26 19:37:45 +0930

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// retuns the text after int c
-
 #include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
